876fce7df5f490abb0b3ba1dc9147f7335be518a
DiegoVT <diego.linguado@hotmail.com>
2025-04-22 21:52:02 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/aluno/ifto/resources/UserResource.java
+++ src/main/java/com/aluno/ifto/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.aluno.ifto.resources;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aluno.ifto.domain.User;
+import com.aluno.ifto.dto.UserDTO;
 import com.aluno.ifto.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){		
+	public ResponseEntity<List<UserDTO>> findAll(){		
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);		
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);		
 		
 	}
 	
