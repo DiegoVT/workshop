73ab1008b53e010e6398feea5cf0dbbf55b67b20
DiegoVT <diego.linguado@hotmail.com>
2025-04-24 17:03:32 -0300

    obtendo um usuario por id

--- src/main/java/com/aluno/ifto/services/UserService.java
+++ src/main/java/com/aluno/ifto/services/UserService.java
@@ -1,12 +1,12 @@
 package com.aluno.ifto.services;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aluno.ifto.domain.User;
 import com.aluno.ifto.repository.UserRepository;
+import com.aluno.ifto.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +18,10 @@ public class UserService {
 		return repo.findAll();
 
 	}
+	
+	public User findById(String id) {
+	    return repo.findById(id)
+	        .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
 }
